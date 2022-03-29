e1d36aa40b773c5e65ee55c9f9f78dd0843686f3
Hadyath <90783881+Hadyath@users.noreply.github.com>
2022-03-28 22:23:42 -0300

    Add files via upload

---
+++ EX5/src/Main/Principal.java
@@ -0,0 +1,13 @@
+package Main;
+
+public class Principal {
+
+	public static void main(String[] args) {
+       Veiculo car;
+       scanner s
+		
+			car = new Veiculo;
+       		car.getMarca()
+	}
+
+}
---
+++ EX5/src/Main/Veiculo.java
@@ -0,0 +1,40 @@
+package Main;
+
+public class Veiculo {
+
+	private String Marca;
+	private String Modelo;
+	private int AnoFabricacao;
+	
+	public Veiculo() {
+		
+	}
+
+	public String getMarca() {
+		return Marca;
+	}
+
+	public void setMarca(String marca) {
+		Marca = marca;
+	}
+
+	public String getModelo() {
+		return Modelo;
+	}
+
+	public void setModelo(String modelo) {
+		Modelo = modelo;
+	}
+
+	public int getAnoFabricacao() {
+		return AnoFabricacao;
+	}
+
+	public void setAnoFabricacao(int anoFabricacao) {
+		AnoFabricacao = anoFabricacao;
+	}
+	
+}
+	
+
+
